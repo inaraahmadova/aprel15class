334d5ada87a94315b4548653d4d7c0631669d107
inaraahmadova <tu7r5hs82@code.edu.az>
2024-04-15 18:06:25 +0400

    first commit

---
+++ aprel15class/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace aprel15class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test test = new Test();
+            //test.Arr[0] = 1;
+            //for (int i = 0; i < test.Arr.Length; i++)
+            //{
+            //    Console.WriteLine(test.Arr[i]);
+            //}
+
+            
+            foreach (var item in typeof(Test).GetProperties())
+            {
+                Console.WriteLine(item.Name);
+            }
+
+
+        }
+        class Test
+        {
+            public int[] Arr { get; set; }
+            public int this[int i]
+            {
+                get
+                {
+                    return Arr[i];
+                }
+                set
+                {
+                    if (value<0)
+                        throw new ArgumentException("deyer 0-dan kicik ola bilmez");
+                    if (i>=Arr.Length) 
+                        throw new IndexOutOfRangeException();
+                }
+            }
+            public Test() 
+            {
+                Arr = new int[10];
+            }
+        }
+    }
+}
